ac734edf4b4342fd80a64b9e7d31c156cb1e31e9
hishinuma616 <dnbwoodleaves3616@gmail.com>
2018-06-07 08:24:06 +0900

    media名前修正、レイアウト修正、cssの圧縮

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
   gulp.src('front/style/scss/*.scss')
   .pipe(sass())
   .pipe(autoprefixer())
+  .pipe(cleanCSS())
   .pipe(gulp.dest('front/style/dst/'));
 });
